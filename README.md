2abfb091c56496429861370850ac6b5c9e8789aa
Wildhoney <adam.timberlake@hubpay.ae>
2025-01-11 21:05:33 +0000

    chore: initial abstraction of the useController functionality

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 <img src="/media/logo.png" />
 
-Strongly typed web component library using generators and efficiently updated views.
+Strongly typed web component library using generators and efficiently updated views alongside the publish-subscribe pattern.
 
 ## Contents
 
@@ -14,12 +14,12 @@ Strongly typed web component library using generators and efficiently updated vi
 
 ## Benefits
 
-- Cleverly architected superset of [Preact](https://github.com/preactjs/preact).
+- Thoughtful event-driven architecture superset of [Preact](https://github.com/preactjs/preact).
 - Ablility to transpile to self-contained web components.
 - Mostly standard JavaScript without quirky rules and exceptions.
 - Clear separation of concerns between business logic and markup.
 - First-class support for skeleton loading using generators with [Immer](https://immerjs.github.io/immer/).
-- Strongly typed routes, styles and controller event-driven architecture.
+- Strongly throughout &ndash; routes, styles, controllers and views.
 - Avoid vendor lock-in with framework agnostic libraries such as [Shoelace](https://shoelace.style/).
 - Easily communicate between controllers using distributed actions.
 - State is mutated sequentially ([FIFO](<https://en.wikipedia.org/wiki/FIFO_(computing_and_electronics)>)) and [deeply merged](#state-merging) for queued mutations.
--- src/library/module/tree/utils.ts
+++ src/library/module/tree/utils.ts
@@ -1,8 +1,18 @@
-import { useEffect, useId, useMemo, useRef, useState } from "preact/hooks";
+import {
+  Dispatch,
+  StateUpdater,
+  useEffect,
+  useId,
+  useMemo,
+  useRef,
+  useState,
+} from "preact/hooks";
 import { Actions, Lifecycle, Model, Routes, State } from "../../types/index.ts";
 import { Props } from "./types.ts";
 import dispatcher from "pubsub-js";
 import { produce } from "immer";
+import { ControllerDefinition } from "../../controller/types.ts";
+import { ModuleOptions } from "../types.ts";
 
 export function useController<
   M extends Model,
@@ -48,36 +58,7 @@ export function useController<
   //   }, []);
 
   useEffect((): void => {
-    const dispatchables = Object.keys(controller).forEach((dispatchable) => {
-      const event = `${id}::${dispatchable}`;
-      dispatcher.subscribe(event, (event, ...properties) => {
-        const resolvers = new Set();
-
-        const generator = controller[dispatchable](...properties);
-
-        while (true) {
-          const result = generator.next(State.Pending);
-
-          if (!result.done) {
-            resolvers.add(result.value());
-            continue;
-          }
-
-          setModel(result.value);
-          break;
-        }
-
-        Promise.allSettled(resolvers).then((resolutions) => {
-          const generator = controller[dispatchable](...properties);
-          generator.next();
-
-          resolutions.forEach((resolution) => {
-            const result = generator.next(resolution.value);
-            if (result.done) setModel(result.value);
-          });
-        });
-      });
-    });
+    observeMethods(id, controller, setModel);
   }, []);
 
   return useMemo(
@@ -87,7 +68,8 @@ export function useController<
         setElement,
         viewActions: {
           dispatch([event, ...properties]: A) {
-            dispatcher.publish(`${id}::${event}`, ...properties);
+            const name = getEventName(id, event);
+            dispatcher.publish(name, ...properties);
           },
           navigate() {},
         },
@@ -95,4 +77,86 @@ export function useController<
     }),
     [model, setElement],
   );
+}
+
+/**
+ * Get the event name of the subscription and dispatches.
+ *
+ * @param id {string}
+ * @param event {string}
+ * @returns {string}
+ */
+function getEventName(id: string, event: string) {
+  return `${id}/${event}`;
+}
+
+/**
+ * Iterate over the controller methods and apply subscription.
+ *
+ * @param id {string}
+ * @param controller {ControllerDefinition<M, A, R>}
+ * @param setModel {Dispatch<StateUpdater<M>>}
+ * @returns {void}
+ */
+function observeMethods<M extends Model, A extends Actions, R extends Routes>(
+  id: string,
+  controller: ControllerDefinition<M, A, R>,
+  setModel: Dispatch<StateUpdater<M>>,
+) {
+  Object.keys(controller).forEach((event) => {
+    const name = getEventName(id, event);
+
+    dispatcher.subscribe(name, (name, ...data: []) => {
+      updateView(controller, name, data, event, setModel);
+    });
+  });
+}
+
+/**
+ * Update the view with the new model from the controller subscription.
+ *
+ * @param controller {ControllerDefinition<M, A, R>}
+ * @param name {string}
+ * @param data {D}
+ * @param dispatchable {string}
+ * @param setModel {Dispatch<StateUpdater<M>>}
+ * @returns {void}
+ */
+function updateView<
+  M extends Model,
+  A extends Actions,
+  R extends Routes,
+  D extends [],
+>(
+  controller: ControllerDefinition<M, A, R>,
+  name: string,
+  data: D,
+  dispatchable: string,
+  setModel: Dispatch<StateUpdater<M>>,
+): void {
+  const resolvers = new Set();
+
+  const generator = controller[dispatchable](...data);
+
+  while (true) {
+    const result = generator.next(State.Pending);
+
+    if (!result.done) {
+      resolvers.add(result.value());
+      continue;
+    }
+
+    setModel(result.value);
+    break;
+  }
+
+  Promise.allSettled(resolvers).then((resolutions) => {
+    const generator = controller[dispatchable](...data);
+    generator.next();
+
+    resolutions.forEach((resolution) => {
+      const result = generator.next(resolution.value);
+      if (result.done) setModel(result.value);
+    });
+  });
 }
