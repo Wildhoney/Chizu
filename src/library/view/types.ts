dc83d3aa262e2095a09ee1c9ac5b4747e5a0c72a
Wildhoney <adam.timberlake@hubpay.ae>
2025-03-13 16:17:16 +0000

    feat: added validator back into view props

--- src/library/view/types.ts
+++ src/library/view/types.ts
@@ -1,5 +1,17 @@
-import { ModuleDefinition } from "../types/index.ts";
+import { Model, ModuleDefinition, State } from "../types/index.ts";
 import * as React from "react";
+
+
+type Helpers = {
+  is(state: State): boolean;
+};
+
+export type Validator<M extends Model> = M extends object
+  ? {
+      [K in keyof M]: M[K] extends object ? Validator<M[K]> & Helpers : Helpers;
+    }
+  : Helpers;
+
 
 export type ViewActions<M extends ModuleDefinition> = {
   dispatch(event: M["Actions"]): void;
@@ -7,6 +19,7 @@ export type ViewActions<M extends ModuleDefinition> = {
 
 export type ViewArgs<M extends ModuleDefinition> = {
   model: M["Model"];
+  validate: Validator<M["Model"]>;
   actions: ViewActions<M>;
 };
 
